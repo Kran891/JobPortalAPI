64856791c75159a519637b6015b2985d4fbb276f
kidev <kirti.devi@inter.ikea.com>
2023-07-31 17:05:52 +0530

    HomeController added

---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobPortal.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
