a219b43bd2a4a5df9d0283f046cf5b90e244abf0
Ravi Teja <Ravi Teja@LAPTOP-O8VKQ9QC>
2023-08-02 21:56:29 +0530

    CompanyModel added

---
+++ Models/CompanyModel.cs
@@ -0,0 +1,11 @@
+﻿namespace JobPortal.Models
+{
+    public class CompanyModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string OwnerId { get; set; }
+        public int CompanyId { get; set; }
+        List<string> CompanyLocations { get; set; }
+    }
+}
