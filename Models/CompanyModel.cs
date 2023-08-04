a6ee2d7deb995d7e4255506eb57acc8eb32d0e31
shubi14 <shubimane1406@gmail.com>
2023-08-04 14:14:08 +0530

    done

--- Models/CompanyModel.cs
+++ Models/CompanyModel.cs
@@ -2,11 +2,11 @@
 {
     public class CompanyModel
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string OwnerId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? OwnerId { get; set; }
         public int CompanyId { get; set; }
-        public string OwnerName { get; set; }
-       public List<string> CompanyLocations { get; set; }
+        public string? OwnerName { get; set; }
+       public List<string>? CompanyLocations { get; set; }
     }
 }
--- Models/JobModel.cs
+++ Models/JobModel.cs
@@ -5,18 +5,18 @@ namespace JobPortal.Models
     public class JobModel
     {
         public int JobId { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
         public int CompanyId { get; set; }
-        public string CompanyName { get; set; }
+        public string? CompanyName { get; set; }
         public double Salary { get; set; }
-        public  List<string> RequiredSkills { get; set; }
+        public  List<string>? RequiredSkills { get; set; }
         public int NoOfApplicants { get;  set; }
-        public List<string> Locations { get;  set; }
+        public List<string>? Locations { get;  set; }
 
         public DateTime InterViewDate { get; set; }
         public DateTime PostedDate { get; set; }
 
-        public string InterViewMode { get; set; }
+        public string? InterViewMode { get; set; }
     }
 }
